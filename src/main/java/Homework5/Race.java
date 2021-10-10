e6ec3ef01121a3dbfa24364ea6c5033cabcaaf22
Vika <ovsannikovav97@gmail.com>
2021-10-10 13:35:57 +0300

    Homework5

---
+++ src/main/java/Homework5/Race.java
@@ -0,0 +1,15 @@
+package Homework5;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class Race {
+    private ArrayList<Stage> stages;
+    private Object mon;
+    public ArrayList<Stage> getStages() { return stages; }
+    public Object getMon() {
+        return mon;
+    }
+    public Race(Stage... stages) {
+        this.stages = new ArrayList<>(Arrays.asList(stages));
+        this.mon = new Object();
+    }
+}
